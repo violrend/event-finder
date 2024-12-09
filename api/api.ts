11eadbd0f9bce7596d963453137d53024a0bff31
Can Gokhan Kiziloglu <can.kiziloglu@gmail.com>
2024-12-09 18:37:21 +0300

    fixed date format when requesting data from API

--- api/api.ts
+++ api/api.ts
@@ -1,6 +1,7 @@
 import { ApiDataType, EventType, SearchParamsType } from '@/lib/types';
 import cities from './cities.json';
 import classifications from './classifications.json';
+import { format } from 'date-fns';
 
 export function getCities(filter?: string) {
   const lowerFilter = filter?.toLocaleLowerCase();
@@ -33,16 +34,17 @@ export const categories = [
   { value: '[KZFzniwnSyZfZ7v7n1, KZFzniwnSyZfZ7v7nl]', label: 'Other' },
 ];
 
-
 export async function fetchEventsAPI(params: SearchParamsType) {
   const { city, category, startDateTime, endDateTime } = params;
   let url = `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=TR&apikey=${process.env.TM_API_KEY}`;
   if (city) url += `&city=${city}`;
   if (category) url += `&classificationId=${category}`;
-  if (startDateTime) url += `&startDateTime=${startDateTime}`;
-  if (endDateTime) url += `&endDateTime=${endDateTime}`;
+  if (startDateTime)
+    url += `&startDateTime=${format(startDateTime, "yyyy-MM-dd'T'HH:mm:ss'Z'")}`;
+  if (endDateTime)
+    url += `&endDateTime=${format(endDateTime, "yyyy-MM-dd'T'HH:mm:ss'Z'")}`;
   console.log(params);
-  console.log(url)
+  console.log(url);
   const response = await fetch(url);
   const data = await response.json();
 
@@ -50,32 +52,35 @@ export async function fetchEventsAPI(params: SearchParamsType) {
     return [];
   }
 
-  const events: EventType[] = data._embedded?.events?.map((event: ApiDataType) => ({
-    id: event.id,
-    name: event.name,
-    url: event.url,
-    images: event.images.map((image: { url: string }) => image.url),
-    date: {
-      localDate: event.dates.start.localDate,
-      localTime: event.dates.start.localTime,
-    },
-    segment: {
-      id: event.classifications[0]?.segment?.id || '',
-      name: event.classifications[0]?.segment?.name || '',
-    },
-    genre: {
-      id: event.classifications[0]?.genre?.id || '',
-      name: event.classifications[0]?.genre?.name || '',
-    },
-    venue: event._embedded.venues[0]?.name || '',
-    city: event._embedded.venues[0]?.city?.name || '',
-    address: event._embedded.venues[0]?.address?.line1 || '',
-    attractions: event._embedded.attractions?.map((attraction) => ({
-      id: attraction.id,
-      name: attraction.name,
-      images: attraction.images.map((image: { url: string }) => image.url),
-    })) || [],
-  }));
-  
+  const events: EventType[] = data._embedded?.events?.map(
+    (event: ApiDataType) => ({
+      id: event.id,
+      name: event.name,
+      url: event.url,
+      images: event.images.map((image: { url: string }) => image.url),
+      date: {
+        localDate: event.dates.start.localDate,
+        localTime: event.dates.start.localTime,
+      },
+      segment: {
+        id: event.classifications[0]?.segment?.id || '',
+        name: event.classifications[0]?.segment?.name || '',
+      },
+      genre: {
+        id: event.classifications[0]?.genre?.id || '',
+        name: event.classifications[0]?.genre?.name || '',
+      },
+      venue: event._embedded.venues[0]?.name || '',
+      city: event._embedded.venues[0]?.city?.name || '',
+      address: event._embedded.venues[0]?.address?.line1 || '',
+      attractions:
+        event._embedded.attractions?.map((attraction) => ({
+          id: attraction.id,
+          name: attraction.name,
+          images: attraction.images.map((image: { url: string }) => image.url),
+        })) || [],
+    })
+  );
+
   return events;
 }
