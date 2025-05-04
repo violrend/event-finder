7e28b173ca221ef35967c897062c9c2ca57d03fb
violrend <95243050+violrend@users.noreply.github.com>
2025-05-04 20:12:18 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,10 @@ export default function RootLayout({
           <main className='flex-grow'>{children}</main>
           <Footer />
         </ThemeProvider>
+
+        
       </body>
+      <script type="text/javascript"> var vglnk = {key: '56f53588b18da5199d57ebb9da80688f'}; (function(d, t) {var s = d.createElement(t); s.type = 'text/javascript';s.async = true; s.src = '//cdn.viglink.com/api/vglnk.js'; var r = d.getElementsByTagName(t)[0]; r.parentNode.insertBefore(s, r); }(document, 'script')); </script>
     </html>
   );
 }
