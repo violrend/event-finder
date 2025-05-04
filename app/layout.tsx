0f7961b1f4a382179447cf294449b7032ed0d712
violrend <95243050+violrend@users.noreply.github.com>
2025-05-04 20:16:48 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
 
         
       </body>
-      <script type="text/javascript"> var vglnk = {key: '56f53588b18da5199d57ebb9da80688f'}; (function(d, t) {var s = d.createElement(t); s.type = 'text/javascript';s.async = true; s.src = '//cdn.viglink.com/api/vglnk.js'; var r = d.getElementsByTagName(t)[0]; r.parentNode.insertBefore(s, r); }(document, 'script')); </script>
     </html>
   );
 }
